bba4d329a84638f61e242d4de1029dd6d8661807
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-07-27 19:56:48 -0300

    blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tulip.mk
+    $(LOCAL_DIR)/bliss_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_tulip-user \
-    aosp_tulip-userdebug \
-    aosp_tulip-eng
+    bliss_tulip-user \
+    bliss_tulip-userdebug \
+    bliss_tulip-eng
