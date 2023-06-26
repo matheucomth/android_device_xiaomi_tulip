237550b6d0876f96b03cb24ce9d8781926031bad
matheucomth <matheussouzas65@outlook.com>
2023-06-26 09:59:23 -0300

    tulip: Bringup to awaken

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tulip.mk
+    $(LOCAL_DIR)/awaken_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_tulip-user \
-    aosp_tulip-userdebug \
-    aosp_tulip-eng
+    awaken_tulip-user \
+    awaken_tulip-userdebug \
+    awaken_tulip-eng
--- awaken_tulip.mk
+++ awaken_tulip.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 # Inherit from tulip device
 $(call inherit-product, device/xiaomi/tulip/device.mk)
 
 # Device Info
-PRODUCT_NAME := aosp_tulip
+PRODUCT_NAME := awaken_tulip
 PRODUCT_DEVICE := tulip
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 6 Pro
@@ -29,3 +29,5 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit some common
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
